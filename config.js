005e0883f0d5939274365197d8bc147d0dec025f
sirdon <inform2theamit@gmail.com>
2022-02-26 13:31:27 +0530

    api added

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 export const API = publicRuntimeConfig.PRODUCTION
   ? "https://blogdeploytest.herokuapp.com/api"
-  : "http://localhost:8000/api";
+  : "https://blogdeploytest.herokuapp.com/api";
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
