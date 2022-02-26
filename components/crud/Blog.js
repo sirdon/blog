0700a2d73dcc55386a92f76e7d0f934b6cf919fe
sirdon <inform2theamit@gmail.com>
2022-02-26 13:07:36 +0530

    builg

--- components/crud/Blog.js
+++ components/crud/Blog.js
@@ -179,6 +179,7 @@ const CreateBlog = ({ router }) => {
               placeholder="Write something..."
               onChange={handleBody}
             />
+
           </div>
           {showSuccess()}
           {showError()}
